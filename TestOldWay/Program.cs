db4591c2f5e5982715caf2f1ddfb72e3f0269477
Daniel Blazevic <daniel.blazevic@alfatherm.com>
2021-11-22 09:39:55 +0100

    Add project files.

---
+++ TestOldWay/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace TestOldWay;
+
+class Program
+{
+    static void SeedData()
+    {
+        var ctx = new Context();
+
+        ctx.Database.EnsureDeleted();
+        ctx.Database.EnsureCreated();
+
+        var post = new Post() { Title = "Post 1" };
+
+        var tag = new Tag() { Text = "Tag 1" };
+
+        ctx.Add(post);
+        ctx.Add(tag);
+
+        ctx.SaveChanges();
+    }
+
+    static void Main(string[] args)
+    {
+        SeedData();
+
+        var ctx = new Context();
+
+        var post = ctx.Posts.First();
+
+        var tag = ctx.Tags.First();
+
+        var postTag = new PostTag() { Post = post, Tag = tag };
+
+        post.PostTags.Add(postTag);
+
+        ctx.ChangeTracker.DetectChanges();
+
+        Console.WriteLine(ctx.ChangeTracker.DebugView.LongView);
+
+        post.PostTags.Remove(postTag);
+
+        ctx.ChangeTracker.DetectChanges();
+
+        Console.WriteLine(ctx.ChangeTracker.DebugView.LongView);
+
+        ctx.SaveChanges();
+    }
+}
